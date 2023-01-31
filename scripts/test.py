6287cd1050a0ba566787ef769da7e024c84fc7ca
jeff7522553 <jeff7522553>
2023-01-31 09:36:06 +0800

    src 位置重新調整

--- scripts/test.py
+++ scripts/test.py
@@ -8,8 +8,7 @@ def add_path(path):
 this_dir = os.path.dirname(__file__)
 this_root = os.path.dirname(this_dir)
 # print('Module    sys.path  ==> ', sys.path)
-add_path(os.path.join(this_root, 'src'))
-
+add_path(this_root)
 
 from gy_redis import RedisManager
 print('12312', RedisManager)
